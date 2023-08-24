1107b2f42f45d9bdf22240e27ba4c5470be9ca89
mwesley8 <toby.wesley@snhu.edu>
2023-08-24 15:11:09 -0500

    Static HTML to Templates With JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,14 @@
+// Object instance of the built in Node file system
+var fs = require('fs');
+
+// Parse the newly created JSON file with resusable static content
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+
 /* Get travel view */
+// Pass the parsed information to the view
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways'});
+    res.render('travel', { title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
