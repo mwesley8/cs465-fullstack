e18133fe436972016bd166f0fc305e2ac80e666b
mwesley8 <toby.wesley@snhu.edu>
2023-09-06 02:25:30 -0500

    Set Up Angular Application Interface

---
+++ app_admin/src/app/data/trips.ts
@@ -0,0 +1,32 @@
+export const trips = [
+    {
+        "code": "GALR210214",
+        "name": "Gale Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-02-14T08:00:00Z",
+        "resort": "Emerald Bay, 3 stars",
+        "perPerson": "799.00",
+        "image": "reef1.jpg",
+        "description": "Hello Matthew. I am able to use 'each' to iterate over the parsed json file."
+    },
+    {
+        "code": "DAWR210315",
+        "name": "Dawson's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-02-15T08:00:00Z",
+        "resort": "Blue Lagoon, 4 stars",
+        "perPerson": "1199.00",
+        "image": "reef2.jpg",
+        "description": "At Dawson's Reef, integer magna leo, posuere et dignissim vitae, porttitor at odio. Pellentesque a metus nec magna placerat volutpat. Nunc nisi mi, elementum sit amet aliquet quis, tristique quis nisl."
+    },
+    {
+        "code": "CLAR210621",
+        "name": "Claire's Reef",
+        "length": "4 nights / 5 days",
+        "start": "2021-06-21T08:00:00Z",
+        "resort": "Coral Sands, 5 stars",
+        "perPerson": "1199.00",
+        "image": "reef3.jpg",
+        "description": "Claire's Reef, donec sed felis risus. Nulla facilisi. Donec a orci tellus, et auctor odio. Fusce ac orci nibh, quis semper arcu. Cras orci neque, euismod et accumsan ac, sagittis molestie lorem. Proin odio sapien, elementum at tempor non."
+    }
+]
